0951d7e4a92046a3671cde23ab2eb0dcc714563a
Ruslan Zaripov <zaripo.rus@gmail.com>
2023-11-25 14:22:01 +0100

    Aktualizacja BE

---
+++ tools-api/bookings/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from bookings.api.views import BookingListAPIView, UpdateBookingAPIView
+
+
+urlpatterns = [
+    path('list', BookingListAPIView.as_view()),
+    path('list/<str:id>', UpdateBookingAPIView.as_view()),
+]
